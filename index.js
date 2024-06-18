900f75c1a655878f07ef11f0c7c5ebb2087b099f
Luz Hernandez <hernandezluz18@gmail.com>
2024-06-18 13:39:56 -0400

    added html and updated scraping functionality

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
 import puppeteer from "puppeteer";
 
 const getQuotes = async () => {
+  // puppeteer session started with a
+  // visible browser-easier to debug
+  // and no default viewport-page will be in full width and height
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
   });
 
+  // open a new page
   const page = await browser.newPage();
 
+  // on new page open to this website and wait the DOM content to be loaded
   await page.goto("http://quotes.toscrape.com/", {
     waitUntil: "domcontentloaded",
   });
 
   let lastPageReached = false;
 
-  while (!lastPageReached) {
+  do {
     const nextPageLink = await page.$(".pager > .next > a");
+    const quotes = await page.evaluate(() => {
+      const quoteList = document.querySelectorAll(".quote");
+      return Array.from(quoteList).map((quote) => {
+        const text = quote.querySelector(".text").innerHTML;
+        const author = quote.querySelector(".author").innerHTML;
+        return { text, author };
+      });
+    });
 
-    if (!nextPageLink) {
-      console.log("No more pages to read.");
-      lastPageReached = true;
-    } else {
+    console.log(quotes.length);
+
+    if (nextPageLink) {
+      // click the next page link
       await nextPageLink.click();
 
+      // wait for navigation to complete
       await page.waitForNavigation();
-
-      const quotes = await page.evaluate(() => {
-        const quoteList = document.querySelectorAll(".quote");
-        return Array.from(quoteList).map((quote) => {
-          const text = quote.querySelector(".text").innerHTML;
-          const author = quote.querySelector(".author").innerHTML;
-          return { text, author };
-        });
-      });
+    } else {
+      console.log("No more pages to read.");
+      lastPageReached = true;
     }
-  }
+  } while (!lastPageReached);
 
-  console.log(quotes);
-  //   await browser.close();
+  // close the browser
+  await browser.close();
 };
 
 getQuotes();
