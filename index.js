ef3b63598744b52a92f09263ffecedd9c9612c03
Luz Hernandez <hernandezluz18@gmail.com>
2024-06-18 11:56:45 -0400

    added logic to scrape all pages

--- index.js
+++ index.js
@@ -11,18 +11,33 @@ const getQuotes = async () => {
   await page.goto("http://quotes.toscrape.com/", {
     waitUntil: "domcontentloaded",
   });
+
+  let lastPageReached = false;
+
+  while (!lastPageReached) {
+    const nextPageLink = await page.$(".pager > .next > a");
+
+    if (!nextPageLink) {
+      console.log("No more pages to read.");
+      lastPageReached = true;
+    } else {
+      await nextPageLink.click();
+
+      await page.waitForNavigation();
+
+      const quotes = await page.evaluate(() => {
+        const quoteList = document.querySelectorAll(".quote");
+        return Array.from(quoteList).map((quote) => {
+          const text = quote.querySelector(".text").innerHTML;
+          const author = quote.querySelector(".author").innerHTML;
+          return { text, author };
+        });
+      });
+    }
+  }
+
+  console.log(quotes);
+  //   await browser.close();
 };
-
-const quotes = await page.evaluate(() => {
-  const quote = document.querySelector(".quote");
-  const text = quote.querySelector(".text").innerHTML;
-  const author = quote.querySelector(".author").innerHTML;
-
-  return { text, author };
-});
-
-console.log(quotes);
-
-await browser.close();
 
 getQuotes();
